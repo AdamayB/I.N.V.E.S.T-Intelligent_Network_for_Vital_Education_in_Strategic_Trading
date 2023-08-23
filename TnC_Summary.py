b595bf73b2db374c8fe7eeee8ce3fb9365a81a11
Adamay Bhardwaj <92214282+AdamayB@users.noreply.github.com>
2023-08-23 22:16:52 +0530

    gradio app

--- TnC_Summary.py
+++ TnC_Summary.py
@@ -1,9 +1,8 @@
-import wikipedia
 from transformers import pipeline
 import gradio as gr
 #tnc=input("Enter Terms and Conditions:")
-title='Terms and Conditions Summarizer'
-description='Paste the terms and conditions here!!'
+title='Terms of Service Summarizer'
+description='Paste in the T&C, if the Terms are too long paste them in parts for better results'
 summerizer=pipeline("summarization")
 
 tncTry='''These Terms of Service reflect the way Google’s business works, the laws that apply to our company, and certain things we’ve always believed to be true. As a result, these Terms of Service help define Google’s relationship with you as you interact with our services. For example, these terms include the following topic headings:
@@ -40,9 +39,15 @@ def summarizer(tncTry):
     return l[0]['summary_text']
 
 theme='HaleyCH/HaleyCH_Theme'
-gr.Interface(fn=summarizer, inputs=["text"], outputs=["text"],
-             # Pass through title and description
-             title=title, description=description,
-             # Set theme and launch parameters
-             theme=theme).launch(server_port=8080, share=True)
 
+
+with gr.Blocks(theme = 'HaleyCH/HaleyCH_Theme') as demo:
+    with gr.Row():
+        with gr.Column(scale=1, min_width=1000):
+            inp = gr.Textbox(placeholder="Paste your Terms of Service text here!")
+            btn = gr.Button("Run")
+            out = gr.Textbox()
+            #btn1.click(lambda x: gr.update(value=''), [], [inp])
+            btn.click(fn=summarizer, inputs=inp, outputs=out)
+
+demo.launch(server_port=8080, share=True)
